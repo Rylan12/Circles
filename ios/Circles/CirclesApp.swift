0f054e39747466980a776f7d1aa47ab526579747
Rylan Polster <rslpolster@gmail.com>
2023-07-10 15:06:29 -0400

    Add initial iOS project

---
+++ ios/Circles/CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesApp.swift
+//  Circles
+//
+//  Created by Rylan Polster on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
