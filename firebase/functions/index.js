761ed49891b2e89ff5a9e79a7dced8f87535650b
Rylan Polster <rslpolster@gmail.com>
2023-07-21 02:39:14 -0400

    Implement cloud function for notifications

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,31 +1,59 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {onCall, HttpsError} = require("firebase-functions/v2/https");
-const {getDatabase} = require("firebase-admin/database");
+const admin = require("firebase-admin");
 const logger = require("firebase-functions/logger");
 
-exports.sendMessage = onCall((request) => {
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.sendMessage = onCall(async (request) => {
+  const timestamp = Date.now();
   const username = request.data.username;
 
   if (username != "abbie" && username != "rylan") {
     throw new HttpsError("failed-precondition", "Invalid username");
   }
 
-  return getDatabase().ref("/messages").push({
-    text: request.data.text,
-    timestamp: Date.now(),
-    author: username,
-    target: username == "abbie" ? "rylan" : "abbie",
-  }).then(() => {
-    logger.info("New message written");
-    return;
+  const recipient = username == "abbie" ? "rylan" : "abbie";
+
+  const senderRef = db.doc(`/users/${username}`);
+  const recipientRef = db.doc(`/users/${recipient}`);
+
+  return Promise.all([
+    senderRef.get(),
+    recipientRef.get()],
+  ).then((snapshots) => {
+    const senderSnapshot = snapshots[0];
+    const recipientSnapshot = snapshots[1];
+
+    const message = senderSnapshot.get("message");
+    const recipientFCMToken = recipientSnapshot.get("token");
+
+    const payload = {
+      token: recipientFCMToken,
+      notification: {
+        body: message,
+      },
+    };
+
+    return admin.messaging().send(payload).then((response) => {
+      logger.info("Successfully sent message:", response);
+
+      return db.doc(`/messages/${timestamp}`).set({
+        message: message,
+        timestamp: timestamp,
+        senderUserName: username,
+        senderRef: senderRef,
+        recipientFCMToken: recipientFCMToken,
+        recipientRef: recipientRef,
+      }).then(() => {
+        logger.info("New message added to database");
+        return {success: true};
+      }).catch((error) => {
+        throw new HttpsError("unknown", error.message, error);
+      });
+    }).catch((error) => {
+      throw new HttpsError("unknown", error.message, error);
+    });
   }).catch((error) => {
     throw new HttpsError("unknown", error.message, error);
   });
